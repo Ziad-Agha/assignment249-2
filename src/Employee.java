e5bd5d890723733931aa1e962c2ee4c518f69e27
Ziad-Agha <zaagha7@gmail.com>
2025-03-20 14:37:12 -0400

    jj

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,59 @@
 
 public class Employee {
-	public static void main(String[] args) {
-		System.out.println(58);
-		String k = "df";
+	private long empNum;
+	private String firstName;
+	private String lastName;
+	private double hrWorked;
+	private double hrlyWage;
+	private double annGrsSalary;
+	public Employee(long empNum, String firstName, String lastName, double hrWorked, double hrlyWage,
+			double annGrsSalary) {
+		this.empNum = empNum;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.hrWorked = hrWorked;
+		this.hrlyWage = hrlyWage;
+		this.annGrsSalary = annGrsSalary;
 	}
+	public long getEmpNum() {
+		return empNum;
+	}
+	public void setEmpNum(long empNum) {
+		this.empNum = empNum;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getHrWorked() {
+		return hrWorked;
+	}
+	public void setHrWorked(double hrWorked) {
+		this.hrWorked = hrWorked;
+	}
+	public double getHrlyWage() {
+		return hrlyWage;
+	}
+	public void setHrlyWage(double hrlyWage) {
+		this.hrlyWage = hrlyWage;
+	}
+	public double getAnnGrsSalary() {
+		return annGrsSalary;
+	}
+	public void setAnnGrsSalary(double annGrsSalary) {
+		this.annGrsSalary = annGrsSalary;
+	}
+	
+	
+	
+	
 
 }
