efbf7ccd2b45be1e7c4c19b516dbd233e04b6351
Abderrahmane102 <bensassi.abderahmane@gmail.com>
2025-03-21 13:08:51 -0400

    Update ProvincialIncomeTax.java

--- src/ProvincialIncomeTax.java
+++ src/ProvincialIncomeTax.java
@@ -1,4 +1,10 @@
 
 public class ProvincialIncomeTax extends Deductions{
-	
+	public static double calculateTax(FileManager employees) {
+		double deductions = 0;
+		
+		for(int i=0; i<employees.employees.getAnnGrsSalary()) {
+			
+		}
+	}
 }
